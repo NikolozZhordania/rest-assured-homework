f290719f94e993d81b1c75bc209c15d5924f80c8
NikolozZhordania <nicholaszhordania@gmail.com>
2026-02-07 05:51:40 +0400

    Rest-Assured Features Homework

--- src/main/java/ge/tbc/testautomation/data/bookstore/Constants.java
+++ src/main/java/ge/tbc/testautomation/data/bookstore/Constants.java
@@ -11,21 +11,10 @@ public class Constants {
 
     public static class Endpoints {
         public static final String GET_BOOKS = "/Books";
-        public static final String GET_BOOK = "/Book";
-        public static final String DELETE_BOOK = "/Book";
     }
 
-    public static class Messages {
-        public static final String NOT_AUTHORIZED_MESSAGE = "User not authorized!";
+    public static class AuthorNames {
+        public static final String FIRST_BOOK_AUTHOR = "Richard E. Silverman";
+        public static final String SECOND_BOOK_AUTHOR = "Addy Osmani";
     }
-
-
-    public static class QueryParams {
-        public static final String ISBN = "ISBN";
-    }
-
-    public static class Limits {
-        public static final int MAX_BOOKS = 2;
-    }
-
 }
--- src/test/java/ge/tbc/testautomation/tests/petstore/PetStoreTests.java
+++ src/test/java/ge/tbc/testautomation/tests/petstore/PetStoreTests.java
@@ -3,92 +3,87 @@ package ge.tbc.testautomation.tests.petstore;
 import ge.tbc.testautomation.basetest.petstore.BaseTest;
 import ge.tbc.testautomation.data.common.StatusCodes;
 import ge.tbc.testautomation.data.petstore.Constants.*;
+import ge.tbc.testautomation.helpers.petstore.Utils;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.json.JSONObject;
 import org.testng.annotations.Test;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 public class PetStoreTests extends BaseTest {
 
     @Test
-    public void createPetOrder() {
-        given()
-                .contentType(ContentType.JSON)
-                .body("""
-                        {
-                            "id": 1001,
-                            "petId": 10,
-                            "quantity": 1,
-                            "shipDate": "2026-02-05T12:00:00.000Z",
-                            "status": "placed",
-                            "complete": true
-                        }
-                    """)
-                .when()
-                .post(Endpoints.CREATE_ORDER)
+    public void addPetAndValidate() {
+        long petId = Utils.createPet(requestSpec, Status.AVAILABLE);
+
+        Response byStatus = given()
+                .spec(requestSpec)
+                .param(JsonKeys.STATUS, Status.AVAILABLE)
+                .get(Endpoints.PET_BY_STATUS)
                 .then()
-                .log().ifValidationFails()
                 .statusCode(StatusCodes.OK)
-                .body("id", notNullValue())
-                .body("petId", equalTo(10))
-                .body("status", equalTo("placed"));
+                .extract().response();
+
+        List<Long> ids = ((List<?>) byStatus.path(JsonKeys.ID)).stream()
+                .map(obj -> ((Number) obj).longValue())
+                .collect(Collectors.toList());
+
+        assertThat(ids, hasItem(petId));
+
+        JSONObject foundPet = new JSONObject(
+                byStatus.jsonPath().getMap(String.format("find { it.id == %d }", petId))
+        );
+
+        assertThat(foundPet.getLong(JsonKeys.ID), equalTo(petId));
+        assertThat(foundPet.getString(JsonKeys.STATUS), equalTo(Status.AVAILABLE));
+        assertThat(foundPet.getString(JsonKeys.NAME), startsWith(PetData.PET_TYPE));
     }
 
     @Test
-    public void updatePetForm() {
+    public void updatePetFormData() {
+        long petId = Utils.createPet(requestSpec, Status.AVAILABLE);
+        String petByIdEndpoint = Endpoints.PET_BY_ID.replace(PathParams.PET_ID, String.valueOf(petId));
+
         given()
+                .spec(requestSpec)
                 .contentType(ContentType.URLENC)
-                .formParam(FormParams.NAME, PetData.PET_NAME)
-                .formParam(FormParams.STATUS, PetData.PET_STATUS_AVAILABLE)
-                .when()
-                .post(Endpoints.UPDATE_PET)
+                .formParam(JsonKeys.NAME, PetData.UPDATED_PET_NAME)
+                .formParam(JsonKeys.STATUS, Status.SOLD)
+                .post(petByIdEndpoint)
                 .then()
-                .log().ifValidationFails()
-                .statusCode(StatusCodes.OK)
-                .body("code", notNullValue())
-                .body("type", notNullValue())
-                .body("message", notNullValue());
-    }
-
-    @Test
-    public void getPetNotFound() {
-        given()
-                .when()
-                .get(Endpoints.GET_PET)
-                .then()
-                .log().ifValidationFails()
-                .statusCode(StatusCodes.NOT_FOUND);
-    }
-
-
-    @Test
-    public void loginUser() {
-        String message =
-                given()
-                        .queryParam(QueryParams.USERNAME, UserInfo.TEST_USERNAME)
-                        .queryParam(QueryParams.PASSWORD, UserInfo.TEST_PASSWORD)
-                        .when()
-                        .get(Endpoints.LOGIN_USER)
-                        .then()
-                        .log().ifValidationFails()
-                        .statusCode(StatusCodes.OK)
-                        .extract().jsonPath().getString("message");
-
-        String number = message.replaceAll("\\D", "");
-        assert number.length() == 10 : "Expected 10 significant digits but found " + number.length();
-        System.out.println("Extracted number: " + number);
-    }
-
-    @Test
-    public void logoutUser() {
-        given()
-                .when()
-                .get(Endpoints.LOGOUT_USER)
-                .then()
-                .log().ifValidationFails()
                 .statusCode(StatusCodes.OK);
+
+        Response response = given()
+                .spec(requestSpec)
+                .get(petByIdEndpoint)
+                .then()
+                .statusCode(StatusCodes.OK)
+                .extract().response();
+
+        assertThat(response.path(JsonKeys.NAME), equalTo(PetData.UPDATED_PET_NAME));
+        assertThat(response.path(JsonKeys.STATUS), equalTo(Status.SOLD));
+    }
+
+    @Test
+    public void uploadPetImage() {
+        long petId = Utils.createPet(requestSpec, Status.AVAILABLE);
+
+        Response response = Utils.uploadPetImage(
+                requestSpec,
+                petId,
+                FilePaths.SEA_OTTER_IMAGE,
+                Metadata.SEA_OTTER
+        );
+
+        assertThat(
+                response.path(JsonKeys.MESSAGE),
+                containsString(Metadata.SEA_OTTER)
+        );
     }
 }
-
-
